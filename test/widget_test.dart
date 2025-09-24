6a8daa71212ea82e01614652a1228d3cffb1349d
Schneidelstrom <jipetilla@up.edu.ph>
2025-09-24 21:35:23 +0800

    LogInPage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:labtrack/main.dart';
+import 'package:labtrack/login.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
